65fb79635d6b9c5758b0da75434d8e45438ff3fd
Esteban Boucher <es.boucher@protomail.com>
2019-10-05 17:14:53 +0200

    Fixed unclickable buttons on the footbar

--- src/components/core/Footer.vue
+++ src/components/core/Footer.vue
@@ -7,15 +7,45 @@
         justify="center"
         no-gutters
       >
-        <v-btn
-          v-for="link in links"
-          :key="link"
+        <v-btn href="/about"
           color="white"
           text
           rounded
           class="my-2"
         >
-          {{ link }}
+        About Us
+        </v-btn>
+        <v-btn href="/about/terms-and-conditions"
+          color="white"
+          text
+          rounded
+          class="my-2"
+        >
+        Terms and Conditions
+        </v-btn>
+        <v-btn href="/about/privacy-policy"
+          color="white"
+          text
+          rounded
+          class="my-2"
+        >
+        Privacy Policy
+        </v-btn>
+        <v-btn href="/about/cookie-policy"
+          color="white"
+          text
+          rounded
+          class="my-2"
+        >
+        Cookie Policy
+        </v-btn>
+        <v-btn href="/about/cookie-consent"
+          color="white"
+          text
+          rounded
+          class="my-2"
+        >
+        Cookie Consent
         </v-btn>
         <v-col
           class="primary lighten-2 py-4 text-center white--text"
@@ -26,17 +56,3 @@
       </v-row>
     </v-footer>
 </template>
-
-<script>
-  export default {
-    data: () => ({
-      links: [
-        'About Us',
-        'Cookie Consent',
-        'Cookie Policy',
-        'Privacy Policy',
-        'Terms and Conditions',
-      ],
-    })
-  }
-</script>
